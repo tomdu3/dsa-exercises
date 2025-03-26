b559e7ba1142cce213e959715e86ca29f258f548
Tomislav Dukez <tomdu3@ymail.com>
2025-03-26 08:30:04 +0000

    update solution move zeros gfg

--- 02-gfg-160/002-problem/solution.js
+++ 02-gfg-160/002-problem/solution.js
@@ -3,8 +3,8 @@ class Solution {
         let count = 0
         for (let index = 0; index < arr.length; index++) {
             if (arr[index] !== 0) {
-                [arr[index], arr[count]] = [arr[count], arr[index]]
-                count++;
+                [arr[index], arr[count]] = [arr[count], arr[index]]  // swap the non-zero element with the zero element
+                count++;  // increment count to move to the index after the last non-zero element
             }
         }
     }
